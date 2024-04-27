0367e80f0378ce81852b9ab2f43e7bc9e510271e
AhmadSaadat <saadatahmad267@gmail.com>
2024-04-27 21:42:18 +0330

    Add comment to source

--- Dadekavan.EndPoint/Program.cs
+++ Dadekavan.EndPoint/Program.cs
@@ -8,6 +8,7 @@ public class Program
     public static void Main(string[] args)
     {
         var services = new ServiceCollection();
+        #region StartTime
         DateTime StartTime;
         DateTime EndTime;
         byte AgencyId;
@@ -15,19 +16,28 @@ public class Program
         StartTime = DateTime.Parse(Console.ReadLine());
         CalculateFlights calculate = new();
         calculate.NewFlights(StartTime);
+        #endregion
 
+        #region EndTime
         Console.WriteLine("Plase Enter  EndDate:");
         EndTime = DateTime.Parse(Console.ReadLine());
         calculate.DiscontinuedFlights(EndTime);
+        #endregion
 
+        #region AgencyId
         Console.WriteLine("Plase Enter  AgencyId:");
         AgencyId = byte.Parse(Console.ReadLine());
+        #endregion
 
+        #region configureService
         services.AddScoped<IFlightReportRepository, FlightReportRepository>();
         services.AddDbContext<DadekavanDbContext>();
         var serviceProvider = services.BuildServiceProvider();
 
         var flightReport = serviceProvider.GetRequiredService<IFlightReportRepository>();
+        #endregion
+
+        //call GetFlightReportListAsync
         flightReport.GetFlightReportListAsync(StartTime, EndTime, AgencyId);
 
         //System.IO.File.WriteAllLines("C:/outFile.csv", flightReport.ToArray());
